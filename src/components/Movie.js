eaf1f98a3f9c96ec65a3d358f4a9778ae9ce5cf1
testerLMJ <78alswo79@gmail.com>
2023-08-17 18:11:29 +0900

    리액트 다이나믹링크이동

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-function Movie({coverImg, title, summary, genres}) {
+function Movie({id, coverImg, title, summary, genres}) {
     return (
         <div>
             <img src={coverImg}/>
-            <Link to="/movie"><h2>{title}</h2></Link>
+            <Link to={`/movie/${id}`}><h2>{title}</h2></Link>
             <p>{summary}</p>
             <ul>
             {genres.map(g =>
@@ -16,11 +16,12 @@ function Movie({coverImg, title, summary, genres}) {
     )
 }
 
-// Movie.PropTypes = {
-//     coverImg: PropTypes.string.isRequired,
-//     title: PropTypes.string.isRequired,
-//     summary: PropTypes.string.isRequired,
-//     genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-// };
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
